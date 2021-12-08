f043fff0d89ba56feb4e0446cf5f445f2034eef2
Borbon Roa, Natalia Andrea <nroa@obsglobal.com>
2021-12-08 16:16:44 -0600

    New code

---
+++ basics-assignment-2-start/basics-assignment-2-start/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  username = '';
+
+  onEmptyString(){
+    this.username = '';
+  }
+
+  onEvaluateString(){
+    if(this.username == ''){
+      return true;
+    }else{return false;}
+  }
+}
+
